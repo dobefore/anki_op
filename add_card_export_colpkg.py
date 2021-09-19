53945e09a51e7484de175cd2ac8c8d23265a1380
dobefore <1432338032@qq.com>
2021-09-19 16:35:34 +0800

    add_export_col

---
+++ add_card_export_colpkg.py
@@ -0,0 +1,39 @@
+
+import tempfile,os,shutil
+from pathlib import Path
+from anki.collection import Collection as aopen
+from anki.exporting import AnkiPackageExporter
+def add_cards_export_col():
+    nam=r'C:\Users\Admin\AppData\Roaming\Anki2\swjz\collection.anki2'
+    col = aopen(nam)
+    # create a new deck
+    newId = col.decks.id("new_deck")
+    # first must create a new note tyoe on anki,
+    # then model name can be accessed
+    m = col.models.by_name('Basic-0cc42')
+    # choose deck for model by did
+    m["did"] =newId
+    col.models.save(m, updateReqs=False)
+    # print deck id and name
+    # print(col.decks.all_names_and_ids())
+    note = col.new_note(m)
+    # card field
+    note["entry"] = "1"
+    note["comment"] = '<img align="baseline" height="100" src="Images/image27372.jpeg" width="100"/>'
+    col.addNote(note)
+    # export collection .colpkg
+    e = AnkiPackageExporter(col)
+    # write to Temp dir
+    fd, newname = tempfile.mkstemp(prefix="ankitest", suffix=".apkg")
+    newname = str(newname)
+    print(newname)
+    os.close(fd)
+    os.unlink(newname)
+    e.exportInto(newname)
+    
+    # cp file to desktop
+    fname=Path(newname).parts[-1]
+    shutil.copyfile(newname,Path(r'C:\Users\Admin\Desktop').joinpath(fname))
+    
+
+
